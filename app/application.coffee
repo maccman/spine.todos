e18984e785beb2ce96116553fd49942c8e1ed005
Alex MacCaw <maccman@gmail.com>
2011-05-11 17:53:03 -1000

    convert to coffeescript

---
+++ app/application.coffee
@@ -0,0 +1,107 @@
+Task = Spine.Model.setup("Task", ["name", "done"]);
+
+Task.extend(Spine.Model.Local);
+
+Task.extend
+  active: ->
+    this.select (item) -> !item.done
+
+  done: ->
+    this.select (item) -> !!item.done
+
+  destroyDone: ->
+    rec.destroy() for rec in this.done()
+
+Tasks = Spine.Controller.create
+  tag: "li"
+  
+  proxied: ["render", "remove"]
+  
+  events:
+   "change   input[type=checkbox]": "toggle",
+   "click    .destroy":             "destroy",
+   "dblclick .view":                "edit",
+   "keypress input[type=text]":     "blurOnEnter",
+   "blur     input[type=text]":     "close"
+ 
+  elements:
+    "input[type=text]": "input",
+    ".item": "wrapper"
+
+  init: ->
+    this.item.bind("update",  this.render)
+    this.item.bind("destroy", this.remove)
+  
+  render: ->
+    elements = $("#taskTemplate").tmpl(this.item)
+    this.el.html(elements)
+    this.refreshElements()
+    this
+  
+  toggle: ->
+    this.item.done = !this.item.done
+    this.item.save()
+  
+  destroy: ->
+    this.item.destroy()
+  
+  edit: ->
+    this.wrapper.addClass("editing")
+    this.input.focus()
+  
+  blurOnEnter: (e) ->
+    if e.keyCode == 13 then e.target.blur()
+  
+  close: ->
+    this.wrapper.removeClass("editing")
+    this.item.updateAttributes({name: this.input.val()})
+  
+  remove: ->
+    this.el.remove()
+
+TaskApp = Spine.Controller.create    
+  proxied: ["addOne", "addAll", "renderCount"]
+
+  events:
+    "submit form":   "create",
+    "click  .clear": "clear"
+
+  elements:
+    ".items":     "items",
+    ".countVal":  "count",
+    ".clear":     "clear",
+    "form input": "input"
+  
+  init: ->
+    Task.bind("create",  this.addOne)
+    Task.bind("refresh", this.addAll)
+    Task.bind("refresh change", this.renderCount)
+    Task.fetch()
+  
+  addOne: (task) ->
+    view = Tasks.init({item: task})
+    this.items.append(view.render().el)
+  
+  addAll: ->
+    Task.each(this.addOne)
+
+  create: (e) ->
+    e.preventDefault()
+    Task.create({name: this.input.val()})
+    this.input.val("")
+  
+  clear: ->
+    Task.destroyDone()
+  
+  renderCount: ->
+    active = Task.active().length
+    this.count.text(active)
+    
+    inactive = Task.done().length
+    if inactive 
+      this.clear.show()
+    else
+      this.clear.hide()
+
+jQuery ->
+  TaskApp.init(el: $("#tasks"))
--- app/application.js
+++ app/application.js
@@ -1,114 +1,133 @@
-jQuery(function($){
-  
-  window.Tasks = Spine.Controller.create({
+(function() {
+  var Task, TaskApp, Tasks;
+  Task = Spine.Model.setup("Task", ["name", "done"]);
+  Task.extend(Spine.Model.Local);
+  Task.extend({
+    active: function() {
+      return this.select(function(item) {
+        return !item.done;
+      });
+    },
+    done: function() {
+      return this.select(function(item) {
+        return !!item.done;
+      });
+    },
+    destroyDone: function() {
+      var rec, _i, _len, _ref, _results;
+      _ref = this.done();
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        rec = _ref[_i];
+        _results.push(rec.destroy());
+      }
+      return _results;
+    }
+  });
+  Tasks = Spine.Controller.create({
     tag: "li",
-    
     proxied: ["render", "remove"],
-    
     events: {
       "change   input[type=checkbox]": "toggle",
-      "click    .destroy":             "destroy",
-      "dblclick .view":                "edit",
-      "keypress input[type=text]":     "blurOnEnter",
-      "blur     input[type=text]":     "close"
+      "click    .destroy": "destroy",
+      "dblclick .view": "edit",
+      "keypress input[type=text]": "blurOnEnter",
+      "blur     input[type=text]": "close"
     },
-    
     elements: {
       "input[type=text]": "input",
       ".item": "wrapper"
     },
-    
-    init: function(){
-      this.item.bind("update",  this.render);
-      this.item.bind("destroy", this.remove);
+    init: function() {
+      this.item.bind("update", this.render);
+      return this.item.bind("destroy", this.remove);
     },
-    
-    render: function(){
-      var elements = $("#taskTemplate").tmpl(this.item);
+    render: function() {
+      var elements;
+      elements = $("#taskTemplate").tmpl(this.item);
       this.el.html(elements);
       this.refreshElements();
       return this;
     },
-    
-    toggle: function(){
+    toggle: function() {
       this.item.done = !this.item.done;
-      this.item.save();      
+      return this.item.save();
     },
-    
-    destroy: function(){
-      this.item.destroy();
+    destroy: function() {
+      return this.item.destroy();
     },
-    
-    edit: function(){
+    edit: function() {
       this.wrapper.addClass("editing");
-      this.input.focus();
+      return this.input.focus();
     },
-    
     blurOnEnter: function(e) {
-      if (e.keyCode == 13) e.target.blur();
+      if (e.keyCode === 13) {
+        return e.target.blur();
+      }
     },
-    
-    close: function(){
+    close: function() {
       this.wrapper.removeClass("editing");
-      this.item.updateAttributes({name: this.input.val()});
+      return this.item.updateAttributes({
+        name: this.input.val()
+      });
     },
-    
-    remove: function(){
-      this.el.remove();
+    remove: function() {
+      return this.el.remove();
     }
   });
-  
-  window.TaskApp = Spine.Controller.create({
-    el: $("#tasks"),
-    
+  TaskApp = Spine.Controller.create({
     proxied: ["addOne", "addAll", "renderCount"],
-
     events: {
-      "submit form":   "create",
+      "submit form": "create",
       "click  .clear": "clear"
     },
-
     elements: {
-      ".items":     "items",
-      ".countVal":  "count",
-      ".clear":     "clear",
+      ".items": "items",
+      ".countVal": "count",
+      ".clear": "clear",
       "form input": "input"
     },
-    
-    init: function(){
-      Task.bind("create",  this.addOne);
+    init: function() {
+      Task.bind("create", this.addOne);
       Task.bind("refresh", this.addAll);
       Task.bind("refresh change", this.renderCount);
-      Task.fetch();
+      return Task.fetch();
     },
-    
     addOne: function(task) {
-      var view = Tasks.init({item: task});
-      this.items.append(view.render().el);
+      var view;
+      view = Tasks.init({
+        item: task
+      });
+      return this.items.append(view.render().el);
     },
-
     addAll: function() {
-      Task.each(this.addOne);
+      return Task.each(this.addOne);
     },
-        
-    create: function(){
-      Task.create({name: this.input.val()});
-      this.input.val("");
-      return false;
+    create: function(e) {
+      e.preventDefault();
+      Task.create({
+        name: this.input.val()
+      });
+      return this.input.val("");
     },
-    
-    clear: function(){
-      Task.destroyDone();
+    clear: function() {
+      return Task.destroyDone();
     },
-    
-    renderCount: function(){
-      var active = Task.active().length;
+    renderCount: function() {
+      var active, inactive;
+      active = Task.active().length;
       this.count.text(active);
-      
-      var inactive = Task.done().length;
-      this.clear[inactive ? "show" : "hide"]();
+      inactive = Task.done().length;
+      if (inactive) {
+        return this.clear.show();
+      } else {
+        return this.clear.hide();
+      }
     }
   });
-  
-  window.App = TaskApp.init();
-});
+  jQuery(function() {
+    return TaskApp.init({
+      el: $("#tasks")
+    });
+  });
+}).call(this);
